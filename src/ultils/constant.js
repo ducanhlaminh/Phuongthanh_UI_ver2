e0f6d256138a5a97511151affc14b75e6209149f
iamfir3 <88335051+iamfir3@users.noreply.github.com>
2022-10-16 14:29:48 +0700

    update

--- src/ultils/constant.js
+++ src/ultils/constant.js
@@ -53,6 +53,20 @@ export const filters = [
   { valueVi: "Giá cao -> thấp", sort: { type: "costPerUnit", code: "DESC" } },
   { valueVi: "Giá thấp -> cao", sort: { type: "costPerUnit", code: "ASC" } },
 ];
+export const filtersSider = [
+  {
+    valueVi: "Stocking",
+    code: 1,
+  },
+  {
+    valueVi: "Stocking",
+    code: 1,
+  },
+  {
+    valueVi: "Stocking",
+    code: 1,
+  },
+];
 export const adminMenu = [
   {
     name: "Tổng sản phẩm",
