87ebb7e398c7e3e3735131f81961dd85837625bb
iamfir3 <88335051+iamfir3@users.noreply.github.com>
2022-10-25 23:17:15 +0700

    commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,26 @@
-import React, { memo } from 'react'
-import icons from '../ultils/icons'
-const { RiSearchLine } = icons
-const Search = () => {
-    return (
-        <div className='flex-auto flex items-center h-[48px]'>
-            <span className='pl-2 bg-[#F1F1F1] h-full flex items-center rounded-l-md'><RiSearchLine size={24} /></span>
-            <input
-                type="text"
-                className='w-full px-5 py-[13px] bg-[#F1F1F1] placeholder:text-gray-800 rounded-r-md outline-none'
-                placeholder='Tìm sản phẩm.....'
-            />
-        </div>
-    )
-}
+import React, { memo, useState, useEffect } from "react";
+import ApiProduct from "../apis/product";
+import icons from "../ultils/icons";
+import SearchingPopup from "./SearchingPopup";
 
-export default memo(Search)
+const { RiSearchLine } = icons;
+const Search = () => {
+  return (
+    <div className="relative">
+      <div className="flex-auto flex items-center h-[48px]">
+        <span className="pl-2 bg-[#F1F1F1] h-full flex items-center rounded-l-md">
+          <RiSearchLine size={24} />
+        </span>
+        <input
+          type="text"
+          className="w-full px-5 py-[13px] bg-[#F1F1F1] placeholder:text-gray-800 rounded-r-md outline-none"
+          placeholder="Tìm sản phẩm....."
+        />
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default memo(Search);
---
+++ src/components/SearchingPopup.js
@@ -0,0 +1,5 @@
+const SearchingPopup=()=>{
+
+}
+
+export default SearchingPopup;
